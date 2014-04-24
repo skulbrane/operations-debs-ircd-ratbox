3af4676f8e1e045db7ad09bb71221ab177b862f7
cpettet <rush@wikimedia.org>
2014-04-24 15:55:41 -0500

    Imported Upstream version 2.2.9
    Change-Id: Idd1555b43e096cfc4f9bd6e0d645665e7111b94e

---
+++ include/_stdint.h
@@ -0,0 +1,9 @@
+#ifndef __INCLUDE__STDINT_H
+#define __INCLUDE__STDINT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H " "
+/* generated using gnu compiler gcc (Ubuntu 4.4.3-4ubuntu5) 4.4.3 */
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
